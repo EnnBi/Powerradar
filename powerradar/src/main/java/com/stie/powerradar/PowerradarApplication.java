c2dac51a5e54b432d349a10fbb7f582770c99b7e
Nadeem Baba <nadym.baba@gmail.com>
2020-04-24 00:55:36 +0530

    aws

--- powerradar/src/main/java/com/stie/powerradar/PowerradarApplication.java
+++ powerradar/src/main/java/com/stie/powerradar/PowerradarApplication.java
@@ -1,5 +1,7 @@
 package com.stie.powerradar;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class PowerradarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PowerradarApplication.class, args);
+		
+		System.getenv().forEach((k,v)->
+		System.err.println(k+"--"+v) 
+		);//System.get("AWS_ACCESS_KEY"));
+		System.err.println(System.getenv("AWS_SECRET_KEY"));
 	}
 
-} 
+}  
