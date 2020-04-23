bd3cda920dd1a7f4aafcb32d8516e75a74ef04be
Nadeem Baba <nadym.baba@gmail.com>
2020-04-24 01:05:03 +0530

    issue

--- powerradar/src/main/java/com/stie/powerradar/PowerradarApplication.java
+++ powerradar/src/main/java/com/stie/powerradar/PowerradarApplication.java
@@ -11,10 +11,10 @@ public class PowerradarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PowerradarApplication.class, args);
 		
-		System.getenv().forEach((k,v)->
+		/*System.getenv().forEach((k,v)->
 		System.err.println(k+"--"+v) 
 		);//System.get("AWS_ACCESS_KEY"));
-		System.err.println(System.getenv("AWS_SECRET_KEY"));
+		System.err.println(System.getenv("AWS_SECRET_KEY"));*/
 	}
 
 }  
